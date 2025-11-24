56e33fed0b01f1b3cf23b7d13522f14e6b9b03e2
ndtribk <ndtribk@gmail.com>
2025-11-24 14:42:25 +0700

    replay_packets.lua

--- examples/scripts/replay_packets.lua
+++
@@ -1,6 +0,0 @@
-local json = require("json")
-
-local seq = json.load_file("../anomaly_sequence.json")
-for i,frame in ipairs(seq.sequence) do
-    print("[Replay] Wheel:", frame.wheel_speed, "| Lidar:", frame.lidar)
-end
